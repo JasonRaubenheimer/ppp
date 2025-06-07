4b0eb7eecf6606de535e0261cc7571a2585d3884
Jason Raubenheimer <fossil_mood.4n@icloud.com>
2025-06-07 08:55:51 +0100

    ch2 drills

--- chapter_02/ch2_drills.cpp
+++ chapter_02/ch2_drills.cpp
@@ -1,5 +1,34 @@
 #include "PPPheaders.h"
 
 int main() {
-    cout << "did this work?\n";
+    string first_name, friend_name;
+    int age;
+    cout << "Enter the name of the person you want to write to: ";
+    cin >> first_name;
+    cout << "Enter the name of another friend: ";
+    cin >> friend_name;
+    cout << "Enter the age of the recipient: ";
+    cin >> age;
+
+    cout << "\n\nDear " << first_name << ",\n\n";
+    cout << "How are you? I am fine. I miss you.\n";
+    cout << "Have you seen " << friend_name << " lately?\n";
+
+    if(age <= 0 || age >= 110) {
+        simple_error("you're kidding!");
+    }
+
+    cout << "I hear you just had your birthday and you are " << age << " years old.\n";
+
+    if(age < 12) {
+        cout << "Next year you will be " << (age+1) << ".\n";
+    } else if(age == 17) {
+        cout << "Next year you will be able to vote.\n";
+    } else if(age > 70) {
+        cout << "Are you retired?\n";
+    }
+
+    cout << "\nYours sincerely.\n\n\n";
+    cout << "Jason\n";
+    
 }
